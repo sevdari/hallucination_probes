92c48d87f73b65f0d97718c608d700be0d126fc9
Tymoteusz Kwiecinski <tkwiecinski@ethz.ch>
2025-12-13 16:15:45 +0100

    llama longfact++

--- generation_pipeline/generation.py
+++ generation_pipeline/generation.py
@@ -11,17 +11,17 @@ from tqdm import tqdm
 MODEL_ID = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 
 # INPUT DATASET
-INPUT_DATASET = "claserken/longfact"
+INPUT_DATASET = "obalcells/longfact-augmented-prompts"
 SPLIT = "train"
 
 # UPLOAD SETTINGS
-TARGET_REPO_ID = "tymciurymciu/base-longfact-generations" 
+TARGET_REPO_ID = "tymciurymciu/longfact-generations" 
 UPLOAD_INTERVAL = 1000 
 SUBSET_NAME = MODEL_ID.split("/")[-1].replace("-", "_")
 
 # OUTPUT
 BASE_OUTPUT_DIR = "/capstor/scratch/cscs/tkwiecinski/hallucination-probes/generation_pipeline/outputs/"
-OUTPUT_FILE = BASE_OUTPUT_DIR + "base_longfact_generations_" + SUBSET_NAME + ".jsonl"
+OUTPUT_FILE = BASE_OUTPUT_DIR + "longfact_generations_" + SUBSET_NAME + ".jsonl"
 
 # GENERATION PARAMETERS
 BATCH_SIZE = 64 # for apertus-8B
