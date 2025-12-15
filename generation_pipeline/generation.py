5447d951efd6193da5963fa03a2a90b22fd1e6a7
Tymoteusz Kwiecinski <tkwiecinski@ethz.ch>
2025-12-15 01:48:23 +0100

    generation

--- generation_pipeline/generation.py
+++ generation_pipeline/generation.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 # ==========================================
 # CONFIG
 # ==========================================
-MODEL_ID = "meta-llama/Meta_Llama_3.1_8B_Instruct"
+MODEL_ID = "meta-llama/Llama-3.1-8B-Instruct"
 
 # INPUT DATASET
 INPUT_DATASET = "obalcells/longfact-augmented-prompts"
